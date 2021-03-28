3284de1d096b5f60265f57848a1b22f2d1ca09da
nuke7 <gombos.marton@icloud.com>
2021-03-29 01:43:50 +0200

    compile hero off, date validation done

--- nasa-fetch/src/components/Search.jsx
+++ nasa-fetch/src/components/Search.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import TextField from "@material-ui/core/TextField";
 import { makeStyles } from "@material-ui/core/styles";
 
@@ -24,24 +24,49 @@ export const Search = () => {
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState([]);
   let searchParams;
+  let dateNow = `${new Date().getFullYear()}-${new Date().getMonth()}-${new Date().getDate()}`;
 
   const fetchData = async () => {
-    setLoading(true);
-    /*    searchParams = `${parseFloat(year)}-${month}-${day}`;
-    console.log(searchParams); */
-    const response = await fetch(
-      `https://api.nasa.gov/planetary/apod?api_key=M61W8ZfNGqjP4EzFr49ctgGmgqnjtR5zIf22qVWD&date=${searchParams}`
-    );
-    setData(await response.json());
-    if (data) {
-      setLoading(false);
+    console.log(searchParams);
+    if (
+      parseInt(searchParams.split("-")[0]) > 1995 &&
+      parseInt(searchParams.split("-")[0]) + parseInt(searchParams.split("-")[1]) <= 2024
+    ) {
+      setLoading(true);
+      /*    searchParams = `${parseFloat(year)}-${month}-${day}`;
+      console.log(searchParams); */
+      const response = await fetch(
+        `https://api.nasa.gov/planetary/apod?api_key=M61W8ZfNGqjP4EzFr49ctgGmgqnjtR5zIf22qVWD&date=${searchParams}`
+      );
+      setData(await response.json());
+
+      if (data) {
+        setLoading(false);
+        console.log(await data);
+      }
+    } else if (searchParams.split("-")[0] < 1996) {
+      alert("only select dates after 1996 january 1st!");
+    } else if (
+      parseInt(searchParams.split("-")[0]) + parseInt(searchParams.split("-")[1]) >
+      2024
+    ) {
+      alert("only select dates before March of 2021!");
     }
   };
 
+  useEffect(() => {
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    searchParams = dateNow;
+
+    fetchData();
+  }, []);
+
   return (
     <div>
-      <div>
+      <div style={{ margin: "1rem" }}>
         <TextField
+          required
+          disableFuture={true}
           onChange={(e) => (searchParams = e.target.value)}
           onBlur={(e) => (searchParams = e.target.value)}
           id="date"
